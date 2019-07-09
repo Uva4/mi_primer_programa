b37c368ea6ab60cca0f860f37a5746dc344585ba
uvaab <uvaablanca@gmail.com>
2019-07-09 05:08:26 +0200

    Ejercicio Adivina al adivino

---
+++ guess_exercise.py
@@ -0,0 +1,28 @@
+
+
+number_to_guess = int(input('Adivina el numero: '))
+
+user_number = int(input('Que numero era?: '))
+
+if number_to_guess == user_number:
+    print('Has acertado!!')
+
+while user_number != number_to_guess:
+    print('Intentalo de nuevo!!')
+    user_number = int(input('Adivina el numero: '))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
